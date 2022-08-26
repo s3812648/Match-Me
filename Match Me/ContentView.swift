f222ccec50cd8fbe94779cf4926856f3011f7655
Dung Nguyen <s3812648@rmit.edu.vn>
2022-08-26 14:17:29 +0700

    Initial Commit

---
+++ Match Me/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Match Me
+//
+//  Created by Dung Nguyen Hung on 26/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Match Me/Match_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Match_MeApp.swift
+//  Match Me
+//
+//  Created by Dung Nguyen Hung on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Match_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
